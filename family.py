bc8f1d0cd7dea4e536e7553a2b065b3f21e60ce9
Chathurinda Ranasinghe <chathurindaranasinghe@gmail.com>
2021-12-26 12:23:51 +0530

    updated

--- family.py
+++ family.py
@@ -2,19 +2,17 @@ import random
 
 
 class Family:
-    newid = 0
+    new_id = 0
 
     def __init__(self):
-        self.memberCount = random.randint(2,7)
+        self.memberCount = random.randint(2, 7)
         self.members = []
 
-        self.id = Family.newid
-        Family.newid += 1
-
-
+        self.id = Family.new_id
+        Family.new_id += 1
 
         # self._infectedMemberCount
-    
-    def addAMember(self, member):
+
+    def add_a_member(self, member):
         self.members.append(member)
 
--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-from population import Population
 from simulation import Simulation
 
-
 sim = Simulation()
-sim.run(2)
-# p = Population()
-# print(f"Adult Count = {getattr(p,'adultCount')}")
-# print(f"Child Count = {getattr(p,'childCount')}")
-# print(f"Man Count = {getattr(p,'manCount')}")
-# print(f"Essential Service Count = {getattr(p,'essentialServiceCount')}")
+sim.run(100)
+sim.save_data()
